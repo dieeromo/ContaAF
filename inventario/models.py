7c87ff8640faee894d76a8cf6ed702e303e8c28d
Diego <diegoromo_408@hotmail.com>
2023-09-15 09:17:35 -0500

    solucion a tablas

--- inventario/models.py
+++ inventario/models.py
@@ -39,7 +39,7 @@ class clientes(models.Model):
 class ingresoFacturas(models.Model):
     idcodigo = models.ForeignKey(codigo_prod, on_delete=models.CASCADE)
     cantidad = models.DecimalField(max_digits=8, decimal_places=2)
-    precio_in = models.DecimalField(max_digits=7, decimal_places=2)
+    precio_in = models.DecimalField(max_digits=8, decimal_places=2)
     idFactura = models.ForeignKey(facturasProveedores, on_delete=models.CASCADE)
     idTipoProducto = models.ForeignKey(tipoProducto, on_delete=models.CASCADE)
     fecha_ingreso = models.DateField()
@@ -47,7 +47,7 @@ class ingresoFacturas(models.Model):
     estadoIngreso = models.BooleanField() #false abierto - true cerrado
     idEstatusUso = models.ForeignKey(nuevo_usado, on_delete=models.CASCADE)
     idBodega = models.ForeignKey(bodega, on_delete=models.CASCADE)
-    precio_factura = models.DecimalField(max_digits=7, decimal_places=2)
+    precio_factura = models.DecimalField(max_digits=8, decimal_places=2)
     observacion = models.CharField(max_length=200, blank=True, null=True)
 
 
@@ -66,14 +66,14 @@ class ingresosRetiros(models.Model):
 class precioSalida(models.Model):
     idcodigo = models.ForeignKey(codigo_prod, on_delete=models.CASCADE)
     idEstatusUso = models.ForeignKey(nuevo_usado, on_delete=models.CASCADE)
-    precio = models.DecimalField(max_digits=5, decimal_places=2)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
     observacion = models.CharField(max_length=70, blank=True, null=True)
     def __str__(self):
         return "{}-{}-{} ".format(self.idcodigo, self.idEstatusUso, self.precio)
 
 class salidaInstalaciones(models.Model):
     idcodigo = models.ForeignKey(codigo_prod, on_delete=models.CASCADE)
-    cantidad = models.DecimalField(max_digits=4, decimal_places=2)
+    cantidad = models.DecimalField(max_digits=6, decimal_places=2)
     idEstatusUso = models.ForeignKey(nuevo_usado, on_delete=models.CASCADE)
     precio = models.ForeignKey(precioSalida,on_delete=models.CASCADE)
     idcliente = models.ForeignKey(clientes, on_delete=models.CASCADE)
@@ -88,7 +88,7 @@ class salidaInstalaciones(models.Model):
 
 class salidaVentasContado(models.Model):
     idcodigo = models.ForeignKey(codigo_prod, on_delete=models.CASCADE)
-    cantidad = models.DecimalField(max_digits=4, decimal_places=2)
+    cantidad = models.DecimalField(max_digits=6, decimal_places=2)
     idEstatusUso = models.ForeignKey(nuevo_usado, on_delete=models.CASCADE)
     precio = models.ForeignKey(precioSalida,on_delete=models.CASCADE)
     idcliente = models.ForeignKey(clientes, on_delete=models.CASCADE)
@@ -114,7 +114,7 @@ class movimimientosInventario(models.Model):
     fecha = models.DateField()
     idcodigo = models.ForeignKey(codigo_prod, on_delete=models.CASCADE)
     idEstatusUso = models.ForeignKey(nuevo_usado, on_delete=models.CASCADE)
-    cantidad = models.DecimalField(max_digits=4, decimal_places=2)
+    cantidad = models.DecimalField(max_digits=6, decimal_places=2)
     digitador = models.ForeignKey(User, on_delete=models.CASCADE)
     observacion = models.CharField(max_length=150, blank=True, null=True)
     creado = models.DateField(auto_now_add=True)
@@ -124,19 +124,19 @@ class movimimientosInventario(models.Model):
 class cierreInventario(models.Model):
     idcodigo = models.ForeignKey(codigo_prod, on_delete=models.CASCADE)
     idEstatusUso = models.ForeignKey(nuevo_usado, on_delete=models.CASCADE)
-    ingreso_proveedores = models.DecimalField(max_digits=4, decimal_places=2)
-    ingreso_retiros = models.DecimalField(max_digits=4, decimal_places=2)
-    salida_instalaciones = models.DecimalField(max_digits=4, decimal_places=2)
-    salida_ventaContado = models.DecimalField(max_digits=4, decimal_places=2)
-    salida_ventaCredito = models.DecimalField(max_digits=4, decimal_places=2)
-    salida_dañados = models.DecimalField(max_digits=4, decimal_places=2)
+    ingreso_proveedores = models.DecimalField(max_digits=6, decimal_places=2)
+    ingreso_retiros = models.DecimalField(max_digits=6, decimal_places=2)
+    salida_instalaciones = models.DecimalField(max_digits=6, decimal_places=2)
+    salida_ventaContado = models.DecimalField(max_digits=6, decimal_places=2)
+    salida_ventaCredito = models.DecimalField(max_digits=6, decimal_places=2)
+    salida_dañados = models.DecimalField(max_digits=6, decimal_places=2)
     idBodega = models.ForeignKey(bodega, on_delete=models.CASCADE)
     id_empresa = models.ForeignKey(empresa, on_delete=models.CASCADE, default=1)
     digitador = models.ForeignKey(User, on_delete=models.CASCADE)
     fecha = models.DateField()
-    movimientos_entrada = models.DecimalField(max_digits=4, decimal_places=2)
-    movimientos_salida = models.DecimalField(max_digits=4, decimal_places=2)
-    total = models.DecimalField(max_digits=4, decimal_places=2)
+    movimientos_entrada = models.DecimalField(max_digits=6, decimal_places=2)
+    movimientos_salida = models.DecimalField(max_digits=6, decimal_places=2)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
     observacion = models.CharField(max_length=100, blank=True, null=True)
 
 class cierreInventario2(models.Model):
